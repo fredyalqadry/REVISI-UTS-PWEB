53c17cd6aed662079b7b5677fd90debbc7c6bdc8
fredyalqadry <90325339+fredyalqadry@users.noreply.github.com>
2021-11-14 19:07:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -328,7 +328,7 @@
             <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
               <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
             </a>
-            <span class="text-muted">© 2021, Fred Al Qadry (2009733)</span>
+            <span class="text-muted">© 2021, Fredy Al Qadry (2009733)</span>
           </div>
         </footer>
       </div>
